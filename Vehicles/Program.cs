c138310ca4b793e2c42fdaef4434ffb1088b03a6
Roberto <Roberto@DESKTOP-1818MJE>
2024-03-01 20:45:02 -0600

    Agregar archivos de proyecto.

---
+++ Vehicles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vehicles;
+
+
+
+
+Car myCar = new Car("Toyota", "Corolla", "Red");
+
+myCar.ShowInfo();
+
+public class Car : Vehicle
+{
+    public string Color { get; set; }
+
+    public Car(string brand, string model, string color) : base (brand, model) 
+    {
+        Color = color;
+       
+    }
+
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine($"Color: {Color}");
+    }
---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Vehicle
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public Vehicle(string brand, string model)
+        {
+            Marca = brand;
+            Modelo = model;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Marca} \nModel: {Modelo}");
+        }
+    }
+}
